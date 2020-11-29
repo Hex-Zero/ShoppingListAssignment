c4e82480d6f52565dce5a7605718e15e6047b71c
Ace Alrich <hex0@live.com>
2020-11-29 23:44:30 +0000

    add labels

--- main.js
+++ main.js
@@ -1,64 +1,64 @@
-const $inputField = $(".max-shopping-list-input")
+const $inputField = $(".max-job-title-input");
 
-const $buttonAddNewItem = $(".max-button")
+const $buttonAddNewItem = $(".max-button");
 
-setCookie("jobList", ["one","two"])
+const jobsArray = getCookie("jobList");
 
-console.log(getCookie("jobList"));
+$inputField.keydown(function (e) {
+  if (e.keyCode === 13) {
+    addNewTask();
+  }
+});
 
+$(":input").on("keyup ", function (e) {
+  if (e.target.value) {
+    $(e.target.labels[0]).addClass("max-show-label");
+  } else {
+    $(e.target.labels[0]).removeClass("max-show-label");
+  }
+});
 
-$inputField.keydown(function(e){
-    if(e.keyCode === 13){
-        addNewTask()
-    }
-})
+function createTaskListItem() {
+  const $taskListItemContainer = $(document.createElement("div")).addClass(
+    "max-task-list-item-container"
+  );
 
-function createTaskListItem(){
-
-    const $taskListItemContainer = $(document.createElement("div"))
-    .addClass("max-task-list-item-container")
-
-    
-    const $taskListItem = $(document.createElement("div"))
+  const $taskListItem = $(document.createElement("div"))
     .addClass("max-shopping-list")
     .html($inputField.val())
-    .click(function(){
-        $checkBox.click();
-    })
-
-    const $checkBox = $(document.createElement("input"))
-    .addClass("max-check-box")
-    .prop("type", "checkbox") 
-    .on("click" , function(){
-        $taskListItem.toggleClass("max-cross-out-item")  
-    })
-
-    const $removeButton = $(document.createElement("button"))
-    .addClass("max-task-item-remove")
-    .html('Delete')
-    .on("click", function(){
-        $taskListItemContainer.remove();
+    .click(function () {
+      $checkBox.click();
     });
 
-   return $taskListItemContainer.append( $checkBox, $taskListItem, $removeButton)
+  const $checkBox = $(document.createElement("input"))
+    .addClass("max-check-box")
+    .prop("type", "checkbox")
+    .on("click", function () {
+      $taskListItem.toggleClass("max-cross-out-item");
+    });
+
+  const $removeButton = $(document.createElement("button"))
+    .addClass("max-task-item-remove")
+    .html("Delete")
+
+    .on("click", function () {
+      $taskListItemContainer.remove();
+    });
+
+  return $taskListItemContainer.append($checkBox, $taskListItem, $removeButton);
 }
 
-function addNewTask(){
-    $(".max-warning-message").remove()
+function addNewTask() {
+  $(".max-all-elements").append(createTaskListItem());
 
-    if(!$inputField.val()){
-        $(".max-input-container")
-        .append("<div class='max-warning-message'>Please Enter Task Name!!!!</div>")
-        return;
-    }
-
-    $(".max-all-elements").append(createTaskListItem())
-
-    $inputField.val("")
+  $inputField.val("");
 }
 
-$buttonAddNewItem.click(function(){
-    addNewTask()
-}); 
+$buttonAddNewItem.click(function () {
+  addNewTask();
+});
 
-
+$(".max-all-elements").submit(function (e) {
+  console.log(e);
+  return false;
+});
