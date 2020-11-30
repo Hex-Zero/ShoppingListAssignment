13c718bdff0a0d2a8ee5d96394058badc394bc98
Ace Alrich <hex0@live.com>
2020-11-30 00:51:06 +0000

    form control and validation custom messages

---
+++ createListItem.js
@@ -0,0 +1,31 @@
+const $inputField = $(".max-job-title-input");
+
+function createTaskListItem() {
+  const $taskListItemContainer = $(document.createElement("div")).addClass(
+    "max-task-list-item-container"
+  );
+
+  const $taskListItem = $(document.createElement("div"))
+    .addClass("max-shopping-list")
+    .html($inputField.val())
+    .click(function () {
+      $checkBox.click();
+    });
+
+  const $checkBox = $(document.createElement("input"))
+    .addClass("max-check-box")
+    .prop("type", "checkbox")
+    .on("click", function () {
+      $taskListItem.toggleClass("max-cross-out-item");
+    });
+
+  const $removeButton = $(document.createElement("button"))
+    .addClass("max-task-item-remove")
+    .html("Delete")
+
+    .on("click", function () {
+      $taskListItemContainer.remove();
+    });
+
+  return $taskListItemContainer.append($checkBox, $taskListItem, $removeButton);
+}
--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const $inputField = $(".max-job-title-input");
-
 const $buttonAddNewItem = $(".max-button");
 
 const jobsArray = getCookie("jobList");
@@ -18,47 +16,13 @@ $(":input").on("keyup ", function (e) {
   }
 });
 
-function createTaskListItem() {
-  const $taskListItemContainer = $(document.createElement("div")).addClass(
-    "max-task-list-item-container"
-  );
-
-  const $taskListItem = $(document.createElement("div"))
-    .addClass("max-shopping-list")
-    .html($inputField.val())
-    .click(function () {
-      $checkBox.click();
-    });
-
-  const $checkBox = $(document.createElement("input"))
-    .addClass("max-check-box")
-    .prop("type", "checkbox")
-    .on("click", function () {
-      $taskListItem.toggleClass("max-cross-out-item");
-    });
-
-  const $removeButton = $(document.createElement("button"))
-    .addClass("max-task-item-remove")
-    .html("Delete")
-
-    .on("click", function () {
-      $taskListItemContainer.remove();
-    });
-
-  return $taskListItemContainer.append($checkBox, $taskListItem, $removeButton);
-}
-
 function addNewTask() {
   $(".max-all-elements").append(createTaskListItem());
 
   $inputField.val("");
 }
 
-$buttonAddNewItem.click(function () {
+$(".max-all-elements").submit(function () {
   addNewTask();
-});
-
-$(".max-all-elements").submit(function (e) {
-  console.log(e);
   return false;
 });
